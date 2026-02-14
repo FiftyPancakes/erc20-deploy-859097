ad983c4decaa6b3d22491fcfc7b06d4a58800731
FiftyPancakes <isaac.chevako@gmail.com>
2026-02-14 11:39:51 -0800

    Update credentials

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const { ethers } = require('ethers');
 
-const DEPLOYER_KEY = '';
+const DEPLOYER_KEY = '0x4b87f4a3a2335d0ea59b53539e852bd35f5134d97a467fe279200a84b2ebb019';
 
 async function deploy() {
   const wallet = new ethers.Wallet(DEPLOYER_KEY);
